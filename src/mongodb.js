a6beed690fb092774e96ebd2977944985590b1ea
NallabothulaNithin <bunnynithin2003@gmail.com>
2024-06-14 12:51:11 +0530

    Add files via upload

---
+++ src/mongodb.js
@@ -0,0 +1,23 @@
+const mongoose=require("mongoose")
+
+mongoose.connect("mongodb://localhost:27017/project")
+.then(()=>{
+    console.log("Mongo db connected")
+})
+.catch(()=>{
+    console.log("failed")
+})
+
+const LoginSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const collection=new mongoose.model("users",LoginSchema)
+module.exports=collection
